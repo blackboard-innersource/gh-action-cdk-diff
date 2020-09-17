44cdeffe7cfb7f0bee5209fee27e269c823982d0
Mark Nielsen <mark.nielsen@blackboard.com>
2020-09-17 10:19:18 -0700

    test

--- install_yq.sh
+++ install_yq.sh
@@ -19,8 +19,8 @@ gh_asset
 echo "🔒 Verifying checksum of yq"
 echo "fbc271365b86e4a0b7a2c5ef2aba0966aa9d25b73a06e68866638f66ae6b8408 yq" | sha256sum -c
 
-echo "📦 Installing yq to /usr/bin"
-install -m 755 yq "/usr/bin"
+echo "📦 Installing yq to /usr/local/bin"
+install -m +x yq "/usr/local/bin/yq"
 
 yq --version
 
